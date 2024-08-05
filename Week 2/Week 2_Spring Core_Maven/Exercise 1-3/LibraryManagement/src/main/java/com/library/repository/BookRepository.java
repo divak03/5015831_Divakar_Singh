53c6e757c4f00dcbfc1062ec848a5f52685260cc
Divakar Singh <158752820+divak03@users.noreply.github.com>
2024-08-06 00:52:13 +0530

    Delete Week 2 directory

--- Week 2/Week 2_Spring Core_Maven/Exercise 1-3/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    // Repository methods
-    public void performRepositoryOperation() {
-        System.out.println("Performing some book repository operations");
-    }
-}
--- Week 2/Week 2_Spring Core_Maven/Exercise 4-8/LibraryManagement1/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class BookRepository {
-    public BookRepository(){
-        System.out.println("This is a Book Repository ");
-    }
-}
