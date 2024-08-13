33173d1bf556cc9be2258025deb8a9a7caf88ca7
Divakar Singh <158752820+divak03@users.noreply.github.com>
2024-08-14 01:12:25 +0530

    Add files via upload

---
+++ Week3/Exercise 1-10/EmployeeManagementSystem/src/main/java/com/library/employeemanagementsystem/repository/DepartmentRepository.java
@@ -0,0 +1,27 @@
+package com.library.employeemanagementsystem.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.library.employeemanagementsystem.entity.Department;
+import com.library.employeemanagementsystem.projection.DepartmentProjection;
+import java.util.List;
+
+
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+    
+    // Using Interface Based Projection
+    @Query("SELECT d FROM Department d")
+    List<DepartmentProjection> findAllDepartments();
+    
+    // Named Query
+    @Query(name="Department.findByName")
+    DepartmentProjection findDepartmentByName(@Param("name") String name);
+    
+    // Custom Query
+    @Query("SELECT d FROM Department d WHERE d.id=:id")
+    DepartmentProjection findDepartmentById(Long id);
+    // End Of Interface Based Projection
+}
